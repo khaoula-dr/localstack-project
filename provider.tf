f03833f87bf7add02ff4dcf11ba5b149ebff94d6
khaoula-dr <driouichkhaoula04@gmail.com>
2026-04-20 11:39:39 +0100

    init: version propre

---
+++ provider.tf
@@ -0,0 +1,26 @@
+terraform {
+	required_providers {
+		aws= {
+			source = "hashicorp/aws"
+			version = "4.67.0"
+		}
+	}
+}
+
+provider "aws" {
+	region                       = "us-east-1"
+	access_key                   = "test"
+	secret_key                   = "test"
+	skip_credentials_validation  = true
+	skip_metadata_api_check      = true
+	skip_requesting_account_id   = true
+
+	endpoints{
+		s3     = "http://localhost:4566"
+		ec2    = "http://localhost:4566"
+		iam    = "http://localhost:4566"
+		sqs    = "http://localhost:4566"
+		lambda = "http://localhost:4566"
+	}
+}
+
